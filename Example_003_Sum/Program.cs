9dd96580ee9409f0931909cab9c541234f4e1417
Mikhail Nikolaev <armavir1308@mail.ru>
2023-01-13 00:24:40 +0300

    add Random Find the summ whith RND

--- Example_003_Sum/Program.cs
+++ Example_003_Sum/Program.cs
@@ -1,5 +1,7 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10); // Sluchainoe chislo 1,2,3, ...9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int rezult = numberA + numberB;
 Console.WriteLine("Сумма двух чисел = ");
 Console.WriteLine(rezult);
